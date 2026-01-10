bf71942e453eef452fd9f057383a03931b3a8d8a
Hiếu <62824420+anhhieuk16@users.noreply.github.com>
2026-01-10 21:05:19 +0700

    update bao cao

--- Repositories/Repositories/VehicleInspectionRepository.cs
+++ Repositories/Repositories/VehicleInspectionRepository.cs
@@ -166,7 +166,14 @@ namespace Repositories.Repositories
                               : 55;
                 var now = DateTime.Now;
                 var expireAt = new DateTime(now.Year, now.Month, now.Day, hours, minutes, 0);
-                ToDate = expireAt;
+                if (ToDate != null)
+                {
+                    ToDate = ((DateTime)ToDate).Date.AddHours(hours).AddMinutes(minutes).AddSeconds(0);
+                }
+                else
+                {
+                    ToDate = expireAt;
+                }
                 return await _VehicleInspectionDAL.CountTotalVehicleInspectionSynthetic(FromDate, ToDate);
             }
             catch (Exception ex)
